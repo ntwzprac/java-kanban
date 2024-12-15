59dcfb050dd195495bc7825d38ddb70f93021c45
ntwzprac <notweuz@yandex.ru>
2024-12-15 16:22:22 +0400

    Исправления Github Actions

--- src/managers/TaskManager.java
+++ src/managers/TaskManager.java
@@ -8,27 +8,39 @@ import java.util.List;
 
 public interface TaskManager {
     List<Task> getTasks();
+
     List<Epic> getEpics();
+
     List<Subtask> getSubtasks();
 
     void deleteTasks();
+
     void deleteEpics();
+
     void deleteSubtasks();
 
     Task getTask(int id);
+
     Epic getEpic(int id);
+
     Subtask getSubtask(int id);
 
     void addTask(Task task);
+
     void addEpic(Epic epic);
+
     void addSubtask(Subtask subtask);
 
     void updateTask(Task task);
+
     void updateEpic(Epic epic);
+
     void updateSubtask(Subtask subtask);
 
     void deleteTask(int id);
+
     void deleteEpic(int id);
+
     void deleteSubtask(int id);
 
     List<Subtask> getEpicSubtasks(int id);
